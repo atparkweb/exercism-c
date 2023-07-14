a3bf60af6841742db3bbaeeed463c150844083c5
Andy Park <atpark333@gmail.com>
2023-07-14 10:08:12 +0900

    Add solution

---
+++ armstrong-numbers/armstrong_numbers.c
@@ -0,0 +1,32 @@
+#include "armstrong_numbers.h"
+#include <stdbool.h>
+#include <math.h>
+
+bool is_armstrong_number(int candidate)
+{
+  if (candidate == 0) return true;
+
+  // Assuming no more than 10 digits (i.e. 32 bit integer max value)
+  int digits[10];
+
+  int numDigits = 0;
+  int number = candidate;
+
+  while (number > 0)
+  {
+	digits[numDigits] = number % 10; // Get the last digit
+	number /= 10;                    // Remove the last digit
+	numDigits++;
+  }
+
+  // All single digit numbers are Armstrong
+  if (numDigits == 1) return true;
+
+  int total = 0;
+  for (int i = 0; i < numDigits; i++)
+  {
+	total += pow(digits[i], numDigits);
+  }
+
+  return candidate == total;
+}
