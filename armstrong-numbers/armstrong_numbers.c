e110b2444ff3eee79e1c07c4c8f0c7778bbad366
Andy Park <atpark333@gmail.com>
2023-07-21 12:09:29 +0900

    Format files using clang-format

--- armstrong-numbers/armstrong_numbers.c
+++ armstrong-numbers/armstrong_numbers.c
@@ -1,36 +1,35 @@
 #include "armstrong_numbers.h"
-#include <stdbool.h>
 #include <math.h>
+#include <stdbool.h>
 
-bool is_armstrong_number(int candidate)
-{
-	if (candidate == 0) return true;
+bool is_armstrong_number(int candidate) {
+  if (candidate == 0)
+    return true;
 
-	// Assuming no more than 10 digits (i.e. 32 bit integer max value)
-	int digits[10];
+  // Assuming no more than 10 digits (i.e. 32 bit integer max value)
+  int digits[10];
 
-	int numDigits = 0;
-	int number = candidate;
+  int numDigits = 0;
+  int number = candidate;
 
-	while (number > 0)
-	{
-		// Get the last digit
-		digits[numDigits] = number % 10;
+  while (number > 0) {
+    // Get the last digit
+    digits[numDigits] = number % 10;
 
-		// Remove the last digit
-		number /= 10;
+    // Remove the last digit
+    number /= 10;
 
-		numDigits++;
-	}
+    numDigits++;
+  }
 
-	// All single digit numbers are Armstrong
-	if (numDigits == 1) return true;
+  // All single digit numbers are Armstrong
+  if (numDigits == 1)
+    return true;
 
-	int total = 0;
-	for (int i = 0; i < numDigits; i++)
-	{
-		total += pow(digits[i], numDigits);
-	}
+  int total = 0;
+  for (int i = 0; i < numDigits; i++) {
+    total += pow(digits[i], numDigits);
+  }
 
-	return candidate == total;
+  return candidate == total;
 }
