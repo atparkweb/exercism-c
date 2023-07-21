62ad97d0ea86456e31d2779090d3b48171029df3
Andy Park <atpark333@gmail.com>
2023-07-21 11:58:11 +0900

    Fix indentation

--- armstrong-numbers/armstrong_numbers.c
+++ armstrong-numbers/armstrong_numbers.c
@@ -4,29 +4,33 @@
 
 bool is_armstrong_number(int candidate)
 {
-  if (candidate == 0) return true;
+	if (candidate == 0) return true;
 
-  // Assuming no more than 10 digits (i.e. 32 bit integer max value)
-  int digits[10];
+	// Assuming no more than 10 digits (i.e. 32 bit integer max value)
+	int digits[10];
 
-  int numDigits = 0;
-  int number = candidate;
+	int numDigits = 0;
+	int number = candidate;
 
-  while (number > 0)
-  {
-	digits[numDigits] = number % 10; // Get the last digit
-	number /= 10;                    // Remove the last digit
-	numDigits++;
-  }
+	while (number > 0)
+	{
+		// Get the last digit
+		digits[numDigits] = number % 10;
 
-  // All single digit numbers are Armstrong
-  if (numDigits == 1) return true;
+		// Remove the last digit
+		number /= 10;
 
-  int total = 0;
-  for (int i = 0; i < numDigits; i++)
-  {
-	total += pow(digits[i], numDigits);
-  }
+		numDigits++;
+	}
 
-  return candidate == total;
+	// All single digit numbers are Armstrong
+	if (numDigits == 1) return true;
+
+	int total = 0;
+	for (int i = 0; i < numDigits; i++)
+	{
+		total += pow(digits[i], numDigits);
+	}
+
+	return candidate == total;
 }
